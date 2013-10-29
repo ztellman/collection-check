d1409ff5fb6cac0ab9918a536443adc392cca531
Zach Tellman <ztellman@gmail.com>
2013-10-29 15:54:50 -0700

    mark 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject collection-check "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject collection-check "0.1.0"
+  :description "fuzz testing for alternate data structures"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[reiddraper/simple-check "0.5.1"]]
