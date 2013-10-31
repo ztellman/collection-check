cda38c47fcaa39ba98e9531708ed16476f83b186
Zach Tellman <ztellman@gmail.com>
2013-10-30 19:03:50 -0700

    mark 0.1.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject collection-check "0.1.1-SNAPSHOT"
+(defproject collection-check "0.1.1"
   :description "fuzz testing for alternate data structures"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[reiddraper/simple-check "0.5.2-SNAPSHOT"]]
+  :dependencies [[reiddraper/simple-check "0.5.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
