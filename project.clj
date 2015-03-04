0e954d854cbe1ff9882a740e783b388d0eb1ff53
Zach Tellman <ztellman@gmail.com>
2015-03-04 15:53:47 -0800

    mark 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject collection-check "0.1.5-SNAPSHOT"
+(defproject collection-check "0.1.5"
   :description "fuzz testing for alternate data structures"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
