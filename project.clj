efd024deb8e2c98c6f161f6ddeaeb9052c29f18b
Nicolas Berger <nicoberger@gmail.com>
2015-08-18 14:42:38 -0300

    Use is instead of assert, so the tests run until the end
    Uses test.chuck to allow using is inside of for-all
    
    It also reports each failed assertion

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "fuzz testing for alternate data structures"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/test.check "0.5.9"]]
+  :dependencies [[org.clojure/test.check "0.8.0"]
+                 [com.gfredericks/test.chuck "0.1.21"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
