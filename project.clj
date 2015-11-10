f8b408732ff6dc5877e4d460bf5307797eb603fb
Martin Klepsch <martinklepsch@googlemail.com>
2015-11-10 18:53:53 +0100

    update to 1.7

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject collection-check "0.1.6"
+(defproject collection-check "0.1.7-SNAPSHOT"
   :description "fuzz testing for alternate data structures"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/test.check "0.8.0"]
                  [com.gfredericks/test.chuck "0.1.21"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
